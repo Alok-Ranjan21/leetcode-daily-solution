e429391a16d4cc41ed81f483692f5aa547e2bf5c
Alok-Ranjan21 <152367873+Alok-Ranjan21@users.noreply.github.com>
2026-03-29 21:38:43 +0530

    Time: 0 ms (100%), Space: 8.8 MB (25.16%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        
+        string even1 = "";
+        string even2 = "";
+        
+        even1 += s1[0];
+        even1 += s1[2];
+        
+        even2 += s2[0];
+        even2 += s2[2];
+        
+        sort(even1.begin(), even1.end());
+        sort(even2.begin(), even2.end());
+        
+        
+        string odd1 = "";
+        string odd2 = "";
+        
+        odd1 += s1[1];
+        odd1 += s1[3];
+        
+        odd2 += s2[1];
+        odd2 += s2[3];
+        
+        sort(odd1.begin(), odd1.end());
+        sort(odd2.begin(), odd2.end());
+        
+        return (even1 == even2) && (odd1 == odd2);
+    }
+};
