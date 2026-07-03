1ac44c1030bac82ec6bc8899a5142b2886c07706
Alok-Ranjan21 <152367873+Alok-Ranjan21@users.noreply.github.com>
2026-07-03 11:42:33 +0530

    Time: 155 ms (46.53%), Space: 165.9 MB (76.24%) - LeetHub

---
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.java
@@ -0,0 +1,105 @@
+import java.util.*;
+
+class Solution {
+
+    public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
+        int n = online.length;
+
+        List<int[]>[] graph = new ArrayList[n];
+        int[] indegree = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int maxCost = 0;
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+
+            graph[u].add(new int[]{v, w});
+            indegree[v]++;
+            maxCost = Math.max(maxCost, w);
+        }
+
+        // Topological Order
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> topo = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                queue.offer(i);
+        }
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            topo.add(u);
+
+            for (int[] edge : graph[u]) {
+                int v = edge[0];
+                indegree[v]--;
+                if (indegree[v] == 0)
+                    queue.offer(v);
+            }
+        }
+
+        int low = 0;
+        int high = maxCost;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canReach(mid, graph, topo, online, k, n)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean canReach(int limit,
+                             List<int[]>[] graph,
+                             List<Integer> topo,
+                             boolean[] online,
+                             long k,
+                             int n) {
+
+        long INF = Long.MAX_VALUE / 4;
+        long[] dist = new long[n];
+        Arrays.fill(dist, INF);
+        dist[0] = 0;
+
+        for (int u : topo) {
+
+            if (dist[u] == INF)
+                continue;
+
+            if (u != 0 && u != n - 1 && !online[u])
+                continue;
+
+            for (int[] edge : graph[u]) {
+
+                int v = edge[0];
+                int w = edge[1];
+
+                if (w < limit)
+                    continue;
+
+                if (v != n - 1 && !online[v])
+                    continue;
+
+                if (dist[v] > dist[u] + w) {
+                    dist[v] = dist[u] + w;
+                }
+            }
+        }
+
+        return dist[n - 1] <= k;
+    }
+}
